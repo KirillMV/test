00536112c92dc70b958838d38f4e3db4c942c6a1
Kirill M <marulin@live.ru>
2023-06-01 12:41:51 +0300

    commitStyle

--- src/components/start/start.jsx
+++ src/components/start/start.jsx
@@ -9,10 +9,12 @@ function Start() {
   const [user, setUser] = useState([]);
 
   function handleSerch() {
+    
     fetch(`https://api.github.com/search/users?q=${login}`)
       .then((response) => response.json())
       .then((posts) => {
         setUser(
+          
           posts.items.map((el) => (
             <Personal
               avatar={el.avatar_url}
@@ -21,20 +23,22 @@ function Start() {
             />
           ))
         );
+        user.forEach(el=>console.log(el.props));
       });
+
   }
 
   return (
-    <div>
-      <input
+    <S.startPage>
+      <S.input
         value={login}
         onChange={(event) => {
           setLogin(event.target.value);
         }}
       />
-      <button onClick={handleSerch}>Поиск</button>
+      <S.buttonSerch onClick={handleSerch}>Поиск</S.buttonSerch>
       <S.userList>{user}</S.userList>
-    </div>
+    </S.startPage>
   );
 }
 
--- src/components/start/start_style.js
+++ src/components/start/start_style.js
@@ -1,14 +1,64 @@
 import styled from "styled-components";
 
+export const startPage = styled.div`
+ margin-left: calc(50vw - 222px);
+  margin-right: calc(50vw - 222px);
+  background: #f5f5f5;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 250px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+background-color:#95afc9;
+`;
 
 
+export const input =styled.input`
+color: #d0cece;
+margin-top:100px;
+width:200px;
+height:30px;
+border-radius:7px;
+  border: none;
+  border-bottom: 1px solid #d0cece;
+  background-color: #f5f5f5;
+  outline: none;
+  
+font-style: normal;
+font-weight: 500;
+font-size: 20px;
+line-height: 14px;
+color:black;
+
+`
+
+export const buttonSerch = styled.div`
+line-height: 40px;
+text-align: center;
+text-transform: uppercase;
+color: #FFFFFF;
+width: 273px;
+height: 40px;
+background: #6699cc;
+border-radius: 7px;
+margin-top:50px;
+margin-bottom:50px;
+`;
 
 export const userList = styled.div`
-width:800px;
-height:500px;
-background-color:palegreen;
+margin-top:40px;
+width:300px;
+height:350px;
+margin-bottom:40px;
+margin-left:80px;
+margin-right:80px;
+background-color:#a6bdd7;
+display:flex;
+flex-direction:column;
+gap:20px;
+font-size: 20px;
+border-radius: 7px;
 overflow-y: scroll;
 &::-webkit-scrollbar { width: 0; }
-
-
 `;
